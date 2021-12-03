df0f7a361c0079fa40c36f27dbd742dbd3619c6e
SimonVentura <vsimone474@gmail.com>
2021-12-03 14:41:44 +0100

    sistuazione secondaria

--- app/src/main/java/com/example/lavoroventura/MainActivity.java
+++ app/src/main/java/com/example/lavoroventura/MainActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listas;
+    String aStati[] = {"italia","francia","germania","Svizzera"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listas=(ListView)findViewById(R.id.listastati);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.activity_main,R.id.listastati,aStati);
+        listas.setAdapter(aa);
     }
 }
