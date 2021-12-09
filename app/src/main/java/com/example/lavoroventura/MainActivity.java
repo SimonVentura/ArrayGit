9f5bd2807d33d8119b818d2d7f7bd3d777848e72
SimonVentura <vsimone474@gmail.com>
2021-12-09 08:47:18 +0100

    situazione ascoltatori

--- app/src/main/java/com/example/lavoroventura/MainActivity.java
+++ app/src/main/java/com/example/lavoroventura/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button bottone,                         //inizializzazione bottone
+
     ListView listas;
     String aStati[] = {"italia","francia","germania","Svizzera"};
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottone = (Button)findViewById(R.id.bottone);   //istanziare un ascoltatore
+        bottone.setOnClickListener(this);
 
         listas=(ListView)findViewById(R.id.listastati);
         ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.activity_main,R.id.listastati,aStati);
